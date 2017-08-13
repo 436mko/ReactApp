3853d9d5c4b5c5df2482e65924cab62b98699897
436mko <martin@Martins-MacBook-Pro.local>
2017-08-13 12:22:56 +0200

    edit copy

--- public/app.jsx
+++ public/app.jsx
@@ -2,7 +2,7 @@ var Greeter = React.createClass({
   render: function() {
     return (
       <div>
-        <h1>Hello React!!!</h1>
+        <h3>Hello React!!!</h3>
         <p>This is from a component!</p>
       </div>
     );
